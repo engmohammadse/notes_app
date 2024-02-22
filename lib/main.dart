ee08a68c91ff0149f56b581195427cc004dd821d
muhammed <mohammadalaaraji@gmail.com>
2024-02-22 16:48:03 +0300

    create notes listview

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,16 @@ import 'package:notes_app/views/notes_view.dart';
 void main() {
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
       home: NotesView(),
     );
-      
-    
   }
 }
---
+++ lib/widgets/custom_note_item.dart
@@ -0,0 +1,60 @@
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: const Color(0xffFFCC80),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter tips',
+              style: TextStyle(fontSize: 26, color: Colors.black),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                'Write your note here, any thing you welid like',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black.withOpacity(.4),
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Padding(
+                padding: const EdgeInsets.only(bottom: 180),
+                child: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 36),
+            child: Text(
+              'Faberury20, 2024',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black.withOpacity(.5),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
